b778ad104235d9cc650013c61cad7fc49837ab2a
naufalfajar-maker <naufalfajarti@gmail.com>
2025-10-18 13:18:41 +0700

    Update template-frontend.php

--- app/Views/template-frontend.php
+++ app/Views/template-frontend.php
@@ -195,13 +195,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <div class="col-sm-6">
             <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item"><a href="#">Layout</a></li>
-              <li class="breadcrumb-item active">Top Navigation</li>
-            </ol>
-          </div><!-- /.col -->
+         
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
