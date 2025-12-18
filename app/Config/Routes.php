5572f11297b692a6e185aad955323553f62404dc
Muhammad Mariffatur Rizky <mariffaturrizky1@gmail.com>
2025-12-18 17:20:52 +0700

    UPDATE MENU SEKOLAH
    This reverts commit ab2ae04965e94cfc0b41c27f05325ac1d410349f.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,16 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-
+$routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('auth', 'Auth::index');
 $routes->get('auth/login', 'Auth::login');          // ✅ route ke halaman login
 $routes->post('auth/cek_login_user', 'Auth::cek_login_user'); // ✅ proses login
 $routes->get('admin', 'Admin::index');
 $routes->get('auth/logout', 'Auth::logout');
-$routes->get('sekolah', 'Sekolah::index');
-$routes->get('sekolah/create', 'Sekolah::create');
-$routes->post('sekolah/insertData', 'Sekolah::insertData');
-$routes->post('sekolah/update', 'Sekolah::update');
-$routes->post('sekolah/delete', 'Sekolah::delete');
-
+$routes->get('Sekolah', 'Sekolah::index');
